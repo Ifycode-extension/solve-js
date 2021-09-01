29812368dd86564c7e1efc82d9ab31db95de6e18
Ifycode <marychord@gmail.com>
2021-09-01 20:30:41 +0100

    Clean up home UI

--- setup/js/app.js
+++ setup/js/app.js
@@ -7,32 +7,36 @@ let filterBtn = document.querySelector('#filterBtn');
 let root = document.querySelector('#root');
 let h1 = document.querySelector('#h1');
 
+let link = document.querySelectorAll('link')[1];
+
 let common = (name, task) => {
     h1.innerHTML = `${name} ${task}`;
+    let linkHref = (path) => {
+        link.href = path;
+    }
     if (h1.innerHTML === 'Solve js ') {
-        root.classList.add('remove');
+        LoadView('./setup/home/home.html');
+        linkHref('./setup/css/js/home.css');
+    }else {
+        linkHref('./setup/css/js/other.css');
     }
 }
 
 common('Solve js', '');
-LoadView('./setup/home/home.html');
 
 homeBtn.addEventListener('click', (e) => {
     e.preventDefault();
     common('Solve js', '');
-    LoadView('./setup/home/home.html');
 });
 
 toggleBtn.addEventListener('click', (e) => {
     e.preventDefault();
     common('Toggle', 'Task');
-    root.classList.remove('remove');
     LoadView('./solve/toggle/toggle.html');
 });
 
 filterBtn.addEventListener('click', (e) => {
     e.preventDefault();
     common('Filter', 'Task');
-    root.classList.remove('remove');
     LoadView('./solve/filter/filter.html');
 });
