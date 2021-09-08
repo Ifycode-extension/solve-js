52f144036ecf2297e951f28c52394268b19ea682
Ifycode <marychord@gmail.com>
2021-09-08 21:56:46 +0100

    Redirect user to index.html from solve folder's html files

---
+++ setup/js/redirect.js
@@ -0,0 +1,10 @@
+/* =============================
+Users are supposed to run      
+solve-js application from the 
+index.html file. Redirect users 
+back to index.html in case they 
+run it from any of the html files 
+in the solve folder - especially
+for vscode live server users
+===============================*/
+window.location.pathname = '/';
