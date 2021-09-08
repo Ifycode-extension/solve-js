f2b309d5d1d839e87e8fcccc9a93421b48dbfe46
Ifycode <marychord@gmail.com>
2021-09-08 19:15:20 +0100

    Make styles consistent across views, and clean up code

--- setup/js/styles.js
+++ setup/js/styles.js
@@ -1,6 +1,38 @@
 let article = document.querySelector('body > article');
 let a = document.querySelectorAll('body > article a');
+let sm = document.querySelector('body > article a#repo-sm');
+let lg = document.querySelector('body > article a#repo-lg');
 
+/* =================================
+Media queries added here too prevent 
+css for home view showing up in other 
+views on page reload - 2 
+===================================*/
+let deviceSizes = [
+    window.matchMedia( "(min-width: 600px)" )
+]
+
+let repoLinksStyle = () => {
+    if (deviceSizes[0].matches) {
+        lg.classList.add('block');
+        sm.classList.remove('block');
+    } else {
+        sm.classList.add('block');
+        lg.classList.remove('block');
+    }
+}
+
+let respondToQuery = () => {
+    repoLinksStyle();
+}
+
+deviceSizes[0].addEventListener('change', respondToQuery);
+
+/* ===================================
+Add these here to prevent css for home 
+view showing up in other views on page 
+reload - 1
+====================================*/
 export const restoreStyle = () => {
     article.classList.remove('none');
     article.classList.add('flex');
@@ -8,6 +40,7 @@ export const restoreStyle = () => {
         tag.classList.remove('none');
         tag.classList.add('block');
     });
+    repoLinksStyle();
 }
 
 export const fixGlitch = () => {
@@ -17,6 +50,7 @@ export const fixGlitch = () => {
         tag.classList.remove('block');
         tag.classList.add('none');
     });
+    repoLinksStyle();
 }
 
 /* From filter.css other-ext */
